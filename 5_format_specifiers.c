2eb40dd5a15fdc548db494b7ac474a722bc88613
pyquant <kyiv@Kyivs-iMac.local>
2022-12-29 06:51:57 -0500

    add format specifiers program script

--- 5_format_specifiers.c
+++ 5_format_specifiers.c
@@ -22,7 +22,20 @@ int main() {
 
    printf("We have %d Apples", apples);
    printf("\n");
-   printf("The current interest rates on bonds with ratings %c is %f", bond_rating, interest_rate);
+   printf("The current interest rates on bonds with ratings %c is %.2f%%\n", bond_rating, interest_rate);
+
+   /*
+   More format specifiers
+   */
+
+   printf ("Characters: %c %c \n", 'a', 65);
+   printf ("Decimals: %d %ld\n", 1977, 650000L);
+   printf ("Preceding with blanks: %10d \n", 1977);
+   printf ("Preceding with zeros: %010d \n", 1977);
+   printf ("Some different radices: %d %x %o %#x %#o \n", 100, 100, 100, 100, 100);
+   printf ("floats: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416);
+   printf ("Width trick: %*d \n", 5, 10);
+   printf ("%s \n", "A string");
 
    return 0;
 
